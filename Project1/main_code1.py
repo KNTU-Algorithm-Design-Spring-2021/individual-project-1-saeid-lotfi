3d2a6a2868f4b27a300bcd7281eec7395339b314
Saeid Lotfi <mehdi.lotfipour91@gmail.com>
2021-03-14 00:50:07 +0330

    Making Code Structure

---
+++ Project1/main_code1.py
@@ -0,0 +1,15 @@
+#min_max function
+#take a list and return its min and max
+def min_max(given_list):
+    pass
+
+#generating random 2d datapoints
+#using numpy
+
+
+#performing min_max function on both features
+#from generated dataset
+
+
+#showing result
+#scatter points and boundry lines
