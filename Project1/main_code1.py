681827753c01f66c5ced7d2714893ce1e4ca9efa
Saeid Lotfi <mehdi.lotfipour91@gmail.com>
2021-03-14 21:42:15 +0330

    Checking Function min_max on Dataset and Plotting

--- Project1/main_code1.py
+++ Project1/main_code1.py
@@ -1,6 +1,7 @@
 #importing libraries
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.patches import Rectangle
 
 #generating random dataset for testing
 #making 10000 points and sampling 100 points
@@ -20,15 +21,18 @@ def min_max(given_list):
             max_list = given_list[i]
             
     return min_list, max_list
-            
-
-#generating random 2d datapoints
-#using numpy
-
 
 #performing min_max function on both features
 #from generated dataset
+hor_min, hor_max = min_max(dataset[:,0])
+ver_min, ver_max = min_max(dataset[:,1])
 
 
 #showing result
 #scatter points and boundry lines
+fig, ax = plt.subplots()
+ax.set_xlim([-5, 5])
+ax.set_ylim([-5, 5])
+ax.scatter(dataset[:,0], dataset[:,1], s= 5, c= 'r')
+ax.add_patch(Rectangle((hor_min, ver_min), hor_max - hor_min, ver_max - ver_min,
+                       edgecolor = 'g', fill= False, lw= 2))
